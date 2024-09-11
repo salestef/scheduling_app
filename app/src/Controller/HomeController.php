047e471dc49ae0e9b62102d9dda3867db4fa2b52
Aleksandar <aleksandar@condor-business.com>
2024-09-11 23:12:26 +0200

    Initial commit

---
+++ app/src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route(
+        path: '/',
+        name: 'home_index',
+        methods: ['GET']
+    )]
+    public function index(): Response
+    {
+        // Render the 'home.html.twig' template
+        return $this->render('home.html.twig');
+    }
+}
