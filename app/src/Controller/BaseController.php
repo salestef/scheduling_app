22322dd6487b71382bf72a6ca8a64410ad8f94a1
Aleksandar <aleksandar@condor-business.com>
2024-09-16 19:17:29 +0200

    Return redirect after login fix

--- app/src/Controller/BaseController.php
+++ app/src/Controller/BaseController.php
@@ -2,20 +2,37 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use App\Form\RegistrationFormType;
 
 class BaseController extends AbstractController
 {
+    private $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
     protected function renderTemplate(string $view, array $parameters = [], Response $response = null): Response
     {
-        // Kreiraj obrazac samo za GET zahteve, inače se koristi obrazac iz parametara
-        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        $request = $this->requestStack->getCurrentRequest();
+        $session = $request ? $request->getSession() : null; // Uvadi sesiju iz Request objekta
+
+        if ($request && $request->getMethod() === 'GET') {
             $form = $this->createForm(RegistrationFormType::class);
             $parameters['registrationForm'] = $form->createView();
+        }
+
+        if (!$this->getUser() && $request) {
+            $session->set('target_path', $request->getUri());
         }
 
         return $this->render($view, $parameters, $response);
     }
 }
+
+
+
+
--- app/src/Security/LoginSuccessHandler.php
+++ app/src/Security/LoginSuccessHandler.php
@@ -8,9 +8,12 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
 class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
 {
+    use TargetPathTrait;
+
     private $urlGenerator;
 
     public function __construct(UrlGeneratorInterface $urlGenerator)
@@ -20,6 +23,9 @@ class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token): RedirectResponse
     {
+        $session = $request->getSession();
+
+        // Proveri da li je korisnik admin i ako jeste, preusmeri ga na backoffice
         if ($token instanceof UsernamePasswordToken) {
             $roles = $token->getUser()->getRoles();
             foreach ($roles as $role) {
@@ -30,9 +36,18 @@ class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
             }
         }
 
-        // Default redirection for non-admin users
-        $url = $this->urlGenerator->generate('booking_index');
+        // Proveri da li postoji zabeleženi target_path u sesiji
+        if ($session->has('target_path')) {
+            $targetPath = $session->get('target_path');
+            $session->remove('target_path');  // Ukloni iz sesije nakon preusmeravanja
+            return new RedirectResponse($targetPath);
+        }
+
+        // Ako nema target_path-a, preusmeri na default korisničku stranicu
+        $url = $this->urlGenerator->generate('home_index');
         return new RedirectResponse($url);
     }
 }
+
+
 
